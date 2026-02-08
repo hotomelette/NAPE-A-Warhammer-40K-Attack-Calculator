b86565beb7c2b9948cb8821764c5826443ba1eff
hotomelette <hotomelette@gmail.com>
2026-02-07 21:00:17 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  plugins: [react()],
+  base: "/NAPE-A-Warhammer-40K-Attack-Calculator/",
+});
+
+export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
